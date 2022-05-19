6454bfe9ac32242e2522ef6da4be2560538ba1cb
Kusnur1234 <kusnur@hyppe.id>
2022-05-19 13:48:10 +0700

    Update TRANS
    Signed-off-by: Kusnur1234 <kusnur@hyppe.id>

--- src/auth/jwt.refresh.strategy.ts
+++ src/auth/jwt.refresh.strategy.ts
@@ -5,24 +5,24 @@ import { AuthService } from "./auth.service";
 
 @Injectable()
 export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh-token') {
-    constructor(
-        @InjectRepository(UserRepository)
-        private userRepository: UserRepository,
-        private authService: AuthService
-    ){
-        super({
-            jwtFromRequest: ExtractJwt.fromBodyField('refresh_token'),
-            secretOrKey: process.env.JWT_REFRESH_TOKEN_SECRET || dbConfig.refreshSecret
-        })
-    }
+    // constructor(
+    //     @InjectRepository(UserRepository)
+    //     private userRepository: UserRepository,
+    //     private authService: AuthService
+    // ){
+    //     super({
+    //         jwtFromRequest: ExtractJwt.fromBodyField('refresh_token'),
+    //         secretOrKey: process.env.JWT_REFRESH_TOKEN_SECRET || dbConfig.refreshSecret
+    //     })
+    // }
 
-    async validate(payload: JwtPayload) {
-        const { username } = payload
-        const user = await this.userRepository.findOne({ username })
+    // async validate(payload: JwtPayload) {
+    //     const { username } = payload
+    //     const user = await this.userRepository.findOne({ username })
 
-        if (!user) {
-            throw new UnauthorizedException()
-        }
-        return user
-    }
+    //     if (!user) {
+    //         throw new UnauthorizedException()
+    //     }
+    //     return user
+    // }
 }
