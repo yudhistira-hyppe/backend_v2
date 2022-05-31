6a7e21afe0e43ab9e29472df3183748b78e49bd3
dkurniawan28 <dkurniawan28@gmail.com>
2022-05-31 14:48:10 +0700

    Delete src directory

--- src/TRANS/jwtrefreshtoken/dto/create-jwtrefreshtoken.dto.ts
+++
@@ -1,9 +0,0 @@
-export class CreateJwtrefreshtokenDto {
-  readonly _id: { oid: String };
-  refresh_token_id: String;
-  email: String;
-  iat: { numberLong: String };
-  exp: { numberLong: String };
-  userAuth: String;
-  _class: String;
-}
