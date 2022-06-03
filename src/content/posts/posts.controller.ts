5d58d47a51f96a0875ed9faa5356f5ef37b36d3e
sukmahyppe <105764151+sukmahyppe@users.noreply.github.com>
2022-06-03 09:48:00 +0700

    update

--- src/content/posts/posts.controller.ts
+++ src/content/posts/posts.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Post,UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+  Req,
+  BadRequestException,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostsDto } from './dto/create-posts.dto';
 import { Posts } from './schemas/posts.schema';
@@ -18,10 +28,39 @@ export class PostsController {
   async findAll(): Promise<Posts[]> {
     return this.PostsService.findAll();
   }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/regcontens')
+  async regContent(): Promise<Object> {
+    return this.PostsService.regcontenMonetize();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/newcontens')
+  async newContent(): Promise<Object> {
+    return this.PostsService.newcontenMonetize();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('/getcontent/')
+  async getContent(@Req() req): Promise<Object> {
+    var email = req.body.email;
+    var type = req.body.type;
+    var type_ = "all";
+    if (email==undefined) {
+      throw new BadRequestException('Unabled to proceed');
+    }
+    if (type != undefined) {
+      type_ = type;
+    }
+    return this.PostsService.getContent(email, type_);
+  }
+
   // @Get(':id')
   // async findOneId(@Param('id') id: string): Promise<Posts> {
   //   return this.PostsService.findOne(id);
   // }
+
   @Get(':email')
   async findOneId(@Param('email') email: string): Promise<Posts> {
     return this.PostsService.findOne(email);
