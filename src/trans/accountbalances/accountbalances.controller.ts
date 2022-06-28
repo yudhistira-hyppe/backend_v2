a420a8b9a47d638b79e2291bc7cf70dc06c3756f
Kusnur1234 <kusnur@hyppe.id>
2022-06-28 15:33:13 +0700

    add wallet history
    Signed-off-by: Kusnur1234 <kusnur@hyppe.id>

--- src/trans/accountbalances/accountbalances.controller.ts
+++ src/trans/accountbalances/accountbalances.controller.ts
@@ -33,4 +33,121 @@ export class AccountbalancesController {
     }
 
 
+    @Post('wallet')
+    @UseGuards(JwtAuthGuard)
+    async searchwallet(@Req() request: Request): Promise<any> {
+        var startdate = null;
+        var enddate = null;
+        var email = null;
+        var data = null;
+        var datasaldo = null;
+        var datapenarikan = null;
+        var iduser = null;
+        var totalsaldo = null;
+        var totalpenarikan = null;
+        var request_json = JSON.parse(JSON.stringify(request.body));
+        if (request_json["email"] !== undefined) {
+            email = request_json["email"];
+            var ubasic = await this.userbasicsService.findOne(email);
+
+            iduser = ubasic._id;
+
+        } else {
+            throw new BadRequestException("Unabled to proceed");
+        }
+
+        if (request_json["startdate"] !== undefined) {
+            startdate = request_json["startdate"];
+        } else {
+            throw new BadRequestException("Unabled to proceed");
+        }
+
+        if (request_json["enddate"] !== undefined) {
+            enddate = request_json["enddate"];
+        } else {
+            throw new BadRequestException("Unabled to proceed");
+        }
+        try {
+            datasaldo = await this.accountbalancesService.findwallettotalsaldo(iduser);
+        } catch (e) {
+            datasaldo = "";
+        }
+
+        try {
+            datapenarikan = await this.accountbalancesService.findwalletpenarikan(iduser, startdate, enddate);
+        } catch (e) {
+            datapenarikan = "";
+        }
+        try {
+            totalsaldo = datasaldo[0].totalsaldo;
+        } catch (e) {
+            totalsaldo = 0;
+        }
+
+        try {
+            totalpenarikan = datapenarikan[0].totalpenarikan;
+        } catch (e) {
+            totalpenarikan = 0;
+        }
+        const messages = {
+            "info": ["The process successful"],
+        };
+
+        data = { "startdate": startdate, "enddate": enddate, "totalsaldo": totalsaldo, "totalpenarikan": totalpenarikan }
+
+        return { response_code: 202, data, messages };
+    }
+
+    @Post('history')
+    @UseGuards(JwtAuthGuard)
+    async searchhistory(@Req() request: Request): Promise<any> {
+        var startdate = null;
+        var enddate = null;
+        var iduser = null;
+        var email = null;
+        var data = null;
+        var skip = null;
+        var limit = null;
+        var request_json = JSON.parse(JSON.stringify(request.body));
+        if (request_json["email"] !== undefined) {
+            email = request_json["email"];
+            var ubasic = await this.userbasicsService.findOne(email);
+
+            iduser = ubasic._id;
+
+        } else {
+            throw new BadRequestException("Unabled to proceed");
+        }
+        if (request_json["startdate"] !== undefined) {
+            startdate = request_json["startdate"];
+        } else {
+            throw new BadRequestException("Unabled to proceed");
+        }
+
+        if (request_json["enddate"] !== undefined) {
+            enddate = request_json["enddate"];
+        } else {
+            throw new BadRequestException("Unabled to proceed");
+        }
+
+        if (request_json["skip"] !== undefined) {
+            skip = request_json["skip"];
+        } else {
+            throw new BadRequestException("Unabled to proceed");
+        }
+        if (request_json["limit"] !== undefined) {
+            limit = request_json["limit"];
+        } else {
+            throw new BadRequestException("Unabled to proceed");
+        }
+        const messages = {
+            "info": ["The process successful"],
+        };
+
+
+        data = await this.accountbalancesService.findwallethistory(iduser, startdate, enddate, skip, limit);
+
+        return { response_code: 202, data, messages };
+    }
+
 }
--- src/trans/accountbalances/accountbalances.service.ts
+++ src/trans/accountbalances/accountbalances.service.ts
@@ -36,4 +36,93 @@ export class AccountbalancesService {
         ]);
         return query;
     }
+
+    async findwalletpenarikan(iduser: object, datestart: string, dateend: string) {
+
+        var currentdate = new Date(new Date(dateend).setDate(new Date(dateend).getDate() + 1));
+
+        var enddate = currentdate.toISOString();
+        const query = await this.accountbalancesModel.aggregate([
+
+            {
+                $match: {
+
+                    "iduser": iduser, "timestamp": { $gte: datestart, $lte: enddate }, "type": "withdraw"
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    totalpenarikan: {
+                        $sum: "$debet"
+                    }
+                }
+            },
+
+
+        ]);
+        return query;
+    }
+
+    async findwallettotalsaldo(iduser: object) {
+        const query = await this.accountbalancesModel.aggregate([
+
+            {
+                $match: {
+
+                    "iduser": iduser
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    totalsaldo: { $sum: { $subtract: ["$kredit", "$debet"] } },
+                }
+            },
+
+
+        ]);
+        return query;
+    }
+
+    async findwallethistory(iduser: object, datestart: string, dateend: string, skip: number, limit: number) {
+
+        var currentdate = new Date(new Date(dateend).setDate(new Date(dateend).getDate() + 1));
+
+        var enddate = currentdate.toISOString();
+        const query = await this.accountbalancesModel.aggregate([
+
+            {
+                $match: {
+
+                    "iduser": iduser, "timestamp": { $gte: datestart, $lte: enddate }
+                }
+            },
+            {
+                $project: {
+                    iduser: "$iduser",
+                    type:
+                    {
+                        $cond: { if: { $eq: ["$type", "withdraw"] }, then: "Buy", else: "Rewards" }
+                    },
+                    timestamp: "$timestamp",
+                    description: "$description",
+                    amount:
+                    {
+                        $cond: { if: { $eq: ["$debet", 0] }, then: "$kredit", else: "$debet" }
+                    },
+                    status:
+                    {
+                        $cond: { if: { $eq: ["$type", "withdraw"] }, then: "SENT", else: "RECEIVE" }
+                    },
+
+
+
+                }
+
+            }, { $skip: skip }, { $limit: limit }
+
+        ]);
+        return query;
+    }
 }
