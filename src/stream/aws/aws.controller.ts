1c5d0a54863186d2e35817ed242eeabad0cebc3e
Kusnur1234 <kusnur@hyppe.id>
2022-07-19 14:39:28 +0700

    update
    Signed-off-by: Kusnur1234 <kusnur@hyppe.id>

--- src/stream/aws/aws.controller.ts
+++ src/stream/aws/aws.controller.ts
@@ -6,15 +6,15 @@ import { AwsRequest } from "./dto/aws.dto";
 
 @Controller()
 export class AwsController {
-    constructor(private readonly awsService: AwsService) {}
+    constructor(private readonly awsService: AwsService) { }
 
     @Post('api/aws/comparing')
-    async comparing(AwsRequest_: AwsRequest)  {
+    async comparing(AwsRequest_: AwsRequest) {
         const data = await this.awsService.comparing(AwsRequest_);
         console.log(data);
     }
 
-    @Post('api/aws/comparing/upload') 
+    @Post('api/aws/comparing/upload')
     @UseInterceptors(FileFieldsInterceptor([
         { name: 'avatar', maxCount: 1 },
         { name: 'background', maxCount: 1 }
@@ -22,10 +22,11 @@ export class AwsController {
     async uploadcomparing(@UploadedFiles() files: { avatar?: Express.Multer.File[], background?: Express.Multer.File[] }) {
         var AwsRequest_ = new AwsRequest();
         AwsRequest_.SimilarityThreshold = 70;
-        AwsRequest_.SourceImage = {Bytes:files.avatar[0].path+'.jpg'};
-        AwsRequest_.TargetImage = {Bytes: files.background[0].path + '.jpg'};
+        AwsRequest_.SourceImage = { Bytes: files.avatar[0].path + '.jpg' };
+        AwsRequest_.TargetImage = { Bytes: files.background[0].path + '.jpg' };
         const data = await this.awsService.comparing(AwsRequest_);
         console.log(data);
-        //console.log(files);
+        // console.log(files);
+        //  console.log(files);
     }
 }
