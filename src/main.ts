a2111ae3a7604e0d89b7307802d18c27897e1843
Kusnur1234 <kusnur@hyppe.id>
2022-08-13 15:44:43 +0700

    update support file
    Signed-off-by: Kusnur1234 <kusnur@hyppe.id>

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   const configService: ConfigService = app.get(ConfigService);
   const adminConfig: ServiceAccount = {
     "projectId": configService.get<string>('FIREBASE_PROJECT_ID'),
-    "privateKey": configService.get<string>('FIREBASE_PRIVATE_KEY') .replace(/\\n/g, '\n'),
+    "privateKey": configService.get<string>('FIREBASE_PRIVATE_KEY').replace(/\\n/g, '\n'),
     "clientEmail": configService.get<string>('FIREBASE_CLIENT_EMAIL'),
   };
   admin.initializeApp({
