5d82380544f1f0512d75183173d2ced5998fc423
sukmahyppe <sukma@hyppe.id>
2022-09-27 13:36:48 +0700

    update

--- src/trans/ads/adsusercompare/adsusercompare.controller.ts
+++ src/trans/ads/adsusercompare/adsusercompare.controller.ts
@@ -144,7 +144,8 @@ export class AdsUserCompareController {
         }
         var data_response = {};
         data_response['adsId'] = data_ads._id.toString();
-        data_response['userID'] = data_userbasic_ads._id.toString();
+        data_response['useradsId'] = data_userads[0]._id.toString();
+        data_response['idUser'] = data_userbasic_ads._id.toString();
         data_response['fullName'] = data_userbasic_ads.fullName;
         data_response['email'] = data_userbasic_ads.email;
         data_response['avartar'] = {
