e88fc058d5ea2f7b494da75fec7ca3c8584a45e2
Kusnur1234 <kusnur@hyppe.id>
2022-10-10 11:12:40 +0700

    update profile
    Signed-off-by: Kusnur1234 <kusnur@hyppe.id>

--- src/trans/profile/profile.controller.ts
+++ src/trans/profile/profile.controller.ts
@@ -30,14 +30,212 @@ export class ProfileController {
     private insightsService: InsightsService,
     private languagesService: LanguagesService,
     private interestsService: InterestsService,
-    private groupService: GroupService, 
-    private divisionService: DivisionService, 
+    private groupService: GroupService,
+    private divisionService: DivisionService,
     private utilsService: UtilsService,
   ) { }
 
 
+  // @Post()
+  // @UseGuards(JwtAuthGuard)
+  // async profileuser(@Req() request: Request): Promise<any> {
+
+
+  //   var request_json = JSON.parse(JSON.stringify(request.body));
+
+  //   var emails = null;
+  //   var interest = [];
+  //   var countries = null;
+  //   var countries_json = null;
+  //   var countri = null;
+  //   var cities_json = null;
+  //   var cities = null;
+  //   var citi = null;
+  //   var mediaprofilepicts_json = null;
+  //   var mediaprofilepicts = null;
+  //   var mediaprofilepicts_res = null;
+  //   var areas = null;
+  //   var insights_json = null;
+  //   var insights_res = null;
+
+  //   var datauserbasicsService = null;
+  //   var ProfileDTO_ = new ProfileDTO();
+  //   if (request_json["email"] !== undefined) {
+  //     emails = request_json["email"];
+  //     datauserbasicsService = await this.userbasicsService.findOne(emails);
+  //     ProfileDTO_ = await this.utilsService.generateProfile(emails, 'LOGIN');
+  //   } else {
+  //     throw new BadRequestException("Unabled to proceed");
+  //   }
+  //   var datagroup = {};
+  //   var datadivision = {};
+
+  //   var group = await this.groupService.findbyuser(datauserbasicsService._id.toString());
+  //   if (group.length>0){
+  //     datagroup = {
+  //         _id: group[0]._id,
+  //         nameGroup: group[0].nameGroup,
+  //       }
+  //     var division = await this.divisionService.findOne(group[0].divisionId.toString());
+  //     if (division.nameDivision !=undefined) {
+  //       datadivision = {
+  //         _id: division._id,
+  //         nameDivision: division.nameDivision,
+  //       }
+  //     }
+  //   }
+  //   ProfileDTO_.group = datagroup;
+  //   ProfileDTO_.division = datadivision;
+
+  //   const messages = {
+  //     "info": ["The process successful"],
+  //   };
+  //   console.log(ProfileDTO_)
+
+  //   const data = [ProfileDTO_];
+  //   // const datauserauthsService = await this.userauthsService.findOne(emails);
+  //   // const datauserbasicsService = await this.userbasicsService.findOne(emails);
+
+  //   // var languages_json = null;
+  //   // if (datauserbasicsService.languages != undefined) {
+  //   //   languages_json = JSON.parse(JSON.stringify(datauserbasicsService.languages));
+  //   // }
+
+  //   // var interest_json = JSON.parse(JSON.stringify(datauserbasicsService.userInterests));
+  //   // try {
+  //   //   insights_json = JSON.parse(JSON.stringify(datauserbasicsService.insight));
+  //   //   var insights = await this.insightsService.findOne(insights_json.$id);
+  //   //   insights_res = {
+  //   //     shares: insights.shares,
+  //   //     followers: insights.followers,
+  //   //     comments: insights.comments,
+  //   //     followings: insights.followings,
+  //   //     reactions: insights.reactions,
+  //   //     posts: insights.posts,
+  //   //     views: insights.views,
+  //   //     likes: insights.likes
+  //   //   };
+  //   // } catch (e) {
+  //   //   insights_res = {
+  //   //     shares: 0,
+  //   //     followers: 0,
+  //   //     comments: 0,
+  //   //     followings: 0,
+  //   //     reactions: 0,
+  //   //     posts: 0,
+  //   //     views: 0,
+  //   //     likes: 0
+  //   //   };
+  //   // }
+  //   // try {
+  //   //   countries_json = JSON.parse(JSON.stringify(datauserbasicsService.countries));
+  //   //   countries = await this.countriesService.findOne(countries_json.$id);
+  //   //   areas = await this.areasService.findOne(countries.countryID);
+  //   //   countri = countries.country;
+  //   // } catch (e) {
+  //   //   countri = "";
+  //   //   areas = "";
+  //   // }
+
+  //   // try {
+  //   //   cities_json = JSON.parse(JSON.stringify(datauserbasicsService.cities));
+  //   //   cities = await this.citiesService.findOne(cities_json.$id);
+  //   //   citi = cities.cityName;
+  //   // } catch (e) {
+  //   //   citi = "";
+  //   // }
+  //   // const languages = await this.languagesService.findOne(languages_json.$id);
+  //   // const interests = await this.interestsService.findOne(interest_json.$id);
+  //   // var datagroup = {};
+  //   // var datadivision = {};
+
+  //   // var group = await this.groupService.findbyuser(datauserbasicsService._id.toString());
+  //   // if (group.length>0){
+  //   //   datagroup = {
+  //   //       _id: group[0]._id,
+  //   //       nameGroup: group[0].nameGroup,
+  //   //     }
+  //   //   var division = await this.divisionService.findOne(group[0].divisionId.toString());
+  //   //   if (division.nameDivision !=undefined) {
+  //   //     datadivision = {
+  //   //       _id: division._id,
+  //   //       nameDivision: division.nameDivision,
+  //   //     }
+  //   //   }
+  //   // }
+
+  //   // try {
+
+  //   //   mediaprofilepicts_json = JSON.parse(JSON.stringify(datauserbasicsService.profilePict));
+  //   //   mediaprofilepicts = await this.mediaprofilepictsService.findOne(mediaprofilepicts_json.$id);
+  //   //   var mediaUri = mediaprofilepicts.mediaUri;
+  //   //   let result = "/profilepict/" + mediaUri.replace("_0001.jpeg", "");
+  //   //   mediaprofilepicts_res = {
+  //   //     mediaBasePath: mediaprofilepicts.mediaBasePath,
+  //   //     mediaUri: mediaprofilepicts.mediaUri,
+  //   //     mediaType: mediaprofilepicts.mediaType,
+  //   //     mediaEndpoint: result
+  //   //   };
+  //   // } catch (e) {
+  //   //   mediaprofilepicts_res = {
+  //   //     mediaBasePath: "",
+  //   //     mediaUri: "",
+  //   //     mediaType: "",
+  //   //     mediaEndpoint: ""
+  //   //   };
+  //   // }
+
+  //   // try {
+  //   //   interest = [{
+  //   //     interestName: interests.interestName,
+  //   //     icon: interests.icon,
+  //   //     createdAt: interests.createdAt,
+  //   //     updatedAt: interests.updatedAt,
+  //   //     _class: interests._class,
+  //   //   }];
+  //   // } catch (err) {
+  //   //   interest = [];
+  //   // }
+  //   // const messages = {
+  //   //   "info": ["The process successful"],
+  //   // };
+
+  //   // const data = [{
+  //   //   "createdAt": datauserbasicsService.createdAt,
+  //   //   "areas": areas,
+  //   //   "group": datagroup,
+  //   //   "division": datadivision,
+  //   //   "country": countri,
+  //   //   "gender": datauserbasicsService.gender,
+  //   //   "idProofNumber": datauserbasicsService.idProofNumber,
+  //   //   "city": citi,
+  //   //   "mobileNumber": datauserbasicsService.mobileNumber,
+  //   //   "roles": datauserauthsService.roles,
+  //   //   "fullName": datauserbasicsService.fullName,
+  //   //   "bio": datauserbasicsService.bio,
+  //   //   "avatar": mediaprofilepicts_res,
+  //   //   "isIdVerified": datauserbasicsService.isIdVerified,
+  //   //   "isEmailVerified": datauserauthsService.isEmailVerified,
+  //   //   "idProofStatus": datauserbasicsService.idProofStatus,
+  //   //   "insight": insights_res,
+  //   //   "langIso": languages.langIso,
+  //   //   "interest": interest,
+  //   //   "dob": datauserbasicsService.dob,
+  //   //   "event": datauserbasicsService.event,
+  //   //   "email": datauserbasicsService.email,
+  //   //   "username": datauserauthsService.username,
+  //   //   "isComplete": datauserbasicsService.isComplete,
+  //   //   "status": datauserbasicsService.status,
+  //   // }];
+
+  //   return {
+  //     response_code: 202,
+  //     data,
+  //     messages
+  //   };
+  // }
+
   @Post()
-  //@FormDataRequest()
   @UseGuards(JwtAuthGuard)
   async profileuser(@Req() request: Request): Promise<any> {
 
@@ -60,174 +258,131 @@ export class ProfileController {
     var insights_res = null;
 
     var datauserbasicsService = null;
-    var ProfileDTO_ = new ProfileDTO();
+
     if (request_json["email"] !== undefined) {
       emails = request_json["email"];
-      datauserbasicsService = await this.userbasicsService.findOne(emails);
-      ProfileDTO_ = await this.utilsService.generateProfile(emails, 'LOGIN');
     } else {
       throw new BadRequestException("Unabled to proceed");
     }
-    var datagroup = {};
-    var datadivision = {};
-
-    var group = await this.groupService.findbyuser(datauserbasicsService._id.toString());
-    if (group.length>0){
-      datagroup = {
-          _id: group[0]._id,
-          nameGroup: group[0].nameGroup,
-        }
-      var division = await this.divisionService.findOne(group[0].divisionId.toString());
-      if (division.nameDivision !=undefined) {
-        datadivision = {
-          _id: division._id,
-          nameDivision: division.nameDivision,
-        }
-      }
-    }
-    ProfileDTO_.group = datagroup;
-    ProfileDTO_.division = datadivision;
 
     const messages = {
       "info": ["The process successful"],
     };
-    console.log(ProfileDTO_)
 
-    const data = [ProfileDTO_];
-    // const datauserauthsService = await this.userauthsService.findOne(emails);
-    // const datauserbasicsService = await this.userbasicsService.findOne(emails);
+    datauserbasicsService = await this.userbasicsService.findOne(emails);
+    var languages_json = null;
+    if (datauserbasicsService.languages != undefined) {
+      languages_json = JSON.parse(JSON.stringify(datauserbasicsService.languages));
+    }
+    var datauserauthsService = await this.userauthsService.findOne(emails);
+    var interest_json = JSON.parse(JSON.stringify(datauserbasicsService.userInterests));
+    try {
+      insights_json = JSON.parse(JSON.stringify(datauserbasicsService.insight));
+      var insights = await this.insightsService.findOne(insights_json.$id);
+      insights_res = {
+        shares: insights.shares,
+        followers: insights.followers,
+        comments: insights.comments,
+        followings: insights.followings,
+        reactions: insights.reactions,
+        posts: insights.posts,
+        views: insights.views,
+        likes: insights.likes
+      };
+    } catch (e) {
+      insights_res = {
+        shares: 0,
+        followers: 0,
+        comments: 0,
+        followings: 0,
+        reactions: 0,
+        posts: 0,
+        views: 0,
+        likes: 0
+      };
+    }
+    try {
+      countries_json = JSON.parse(JSON.stringify(datauserbasicsService.countries));
+      countries = await this.countriesService.findOne(countries_json.$id);
+      areas = await this.areasService.findOne(countries.countryID);
+      countri = countries.country;
+    } catch (e) {
+      countri = "";
+      areas = "";
+    }
 
-    // var languages_json = null;
-    // if (datauserbasicsService.languages != undefined) {
-    //   languages_json = JSON.parse(JSON.stringify(datauserbasicsService.languages));
-    // }
+    try {
+      cities_json = JSON.parse(JSON.stringify(datauserbasicsService.cities));
+      cities = await this.citiesService.findOne(cities_json.$id);
+      citi = cities.cityName;
+    } catch (e) {
+      citi = "";
+    }
+    const languages = await this.languagesService.findOne(languages_json.$id);
+    const interests = await this.interestsService.findOne(interest_json.$id);
+    var datagroup = {};
+    var datadivision = {};
 
-    // var interest_json = JSON.parse(JSON.stringify(datauserbasicsService.userInterests));
-    // try {
-    //   insights_json = JSON.parse(JSON.stringify(datauserbasicsService.insight));
-    //   var insights = await this.insightsService.findOne(insights_json.$id);
-    //   insights_res = {
-    //     shares: insights.shares,
-    //     followers: insights.followers,
-    //     comments: insights.comments,
-    //     followings: insights.followings,
-    //     reactions: insights.reactions,
-    //     posts: insights.posts,
-    //     views: insights.views,
-    //     likes: insights.likes
-    //   };
-    // } catch (e) {
-    //   insights_res = {
-    //     shares: 0,
-    //     followers: 0,
-    //     comments: 0,
-    //     followings: 0,
-    //     reactions: 0,
-    //     posts: 0,
-    //     views: 0,
-    //     likes: 0
-    //   };
-    // }
-    // try {
-    //   countries_json = JSON.parse(JSON.stringify(datauserbasicsService.countries));
-    //   countries = await this.countriesService.findOne(countries_json.$id);
-    //   areas = await this.areasService.findOne(countries.countryID);
-    //   countri = countries.country;
-    // } catch (e) {
-    //   countri = "";
-    //   areas = "";
-    // }
+    try {
 
-    // try {
-    //   cities_json = JSON.parse(JSON.stringify(datauserbasicsService.cities));
-    //   cities = await this.citiesService.findOne(cities_json.$id);
-    //   citi = cities.cityName;
-    // } catch (e) {
-    //   citi = "";
-    // }
-    // const languages = await this.languagesService.findOne(languages_json.$id);
-    // const interests = await this.interestsService.findOne(interest_json.$id);
-    // var datagroup = {};
-    // var datadivision = {};
+      mediaprofilepicts_json = JSON.parse(JSON.stringify(datauserbasicsService.profilePict));
+      mediaprofilepicts = await this.mediaprofilepictsService.findOne(mediaprofilepicts_json.$id);
+      var mediaUri = mediaprofilepicts.mediaUri;
+      let result = "/profilepict/" + mediaUri.replace("_0001.jpeg", "");
+      mediaprofilepicts_res = {
+        mediaBasePath: mediaprofilepicts.mediaBasePath,
+        mediaUri: mediaprofilepicts.mediaUri,
+        mediaType: mediaprofilepicts.mediaType,
+        mediaEndpoint: result
+      };
+    } catch (e) {
+      mediaprofilepicts_res = {
+        mediaBasePath: "",
+        mediaUri: "",
+        mediaType: "",
+        mediaEndpoint: ""
+      };
+    }
 
-    // var group = await this.groupService.findbyuser(datauserbasicsService._id.toString());
-    // if (group.length>0){
-    //   datagroup = {
-    //       _id: group[0]._id,
-    //       nameGroup: group[0].nameGroup,
-    //     }
-    //   var division = await this.divisionService.findOne(group[0].divisionId.toString());
-    //   if (division.nameDivision !=undefined) {
-    //     datadivision = {
-    //       _id: division._id,
-    //       nameDivision: division.nameDivision,
-    //     }
-    //   }
-    // }
+    try {
+      interest = [{
+        interestName: interests.interestName,
+        icon: interests.icon,
+        createdAt: interests.createdAt,
+        updatedAt: interests.updatedAt,
+        _class: interests._class,
+      }];
+    } catch (err) {
+      interest = [];
+    }
 
-    // try {
-
-    //   mediaprofilepicts_json = JSON.parse(JSON.stringify(datauserbasicsService.profilePict));
-    //   mediaprofilepicts = await this.mediaprofilepictsService.findOne(mediaprofilepicts_json.$id);
-    //   var mediaUri = mediaprofilepicts.mediaUri;
-    //   let result = "/profilepict/" + mediaUri.replace("_0001.jpeg", "");
-    //   mediaprofilepicts_res = {
-    //     mediaBasePath: mediaprofilepicts.mediaBasePath,
-    //     mediaUri: mediaprofilepicts.mediaUri,
-    //     mediaType: mediaprofilepicts.mediaType,
-    //     mediaEndpoint: result
-    //   };
-    // } catch (e) {
-    //   mediaprofilepicts_res = {
-    //     mediaBasePath: "",
-    //     mediaUri: "",
-    //     mediaType: "",
-    //     mediaEndpoint: ""
-    //   };
-    // }
-
-    // try {
-    //   interest = [{
-    //     interestName: interests.interestName,
-    //     icon: interests.icon,
-    //     createdAt: interests.createdAt,
-    //     updatedAt: interests.updatedAt,
-    //     _class: interests._class,
-    //   }];
-    // } catch (err) {
-    //   interest = [];
-    // }
-    // const messages = {
-    //   "info": ["The process successful"],
-    // };
-
-    // const data = [{
-    //   "createdAt": datauserbasicsService.createdAt,
-    //   "areas": areas,
-    //   "group": datagroup,
-    //   "division": datadivision,
-    //   "country": countri,
-    //   "gender": datauserbasicsService.gender,
-    //   "idProofNumber": datauserbasicsService.idProofNumber,
-    //   "city": citi,
-    //   "mobileNumber": datauserbasicsService.mobileNumber,
-    //   "roles": datauserauthsService.roles,
-    //   "fullName": datauserbasicsService.fullName,
-    //   "bio": datauserbasicsService.bio,
-    //   "avatar": mediaprofilepicts_res,
-    //   "isIdVerified": datauserbasicsService.isIdVerified,
-    //   "isEmailVerified": datauserauthsService.isEmailVerified,
-    //   "idProofStatus": datauserbasicsService.idProofStatus,
-    //   "insight": insights_res,
-    //   "langIso": languages.langIso,
-    //   "interest": interest,
-    //   "dob": datauserbasicsService.dob,
-    //   "event": datauserbasicsService.event,
-    //   "email": datauserbasicsService.email,
-    //   "username": datauserauthsService.username,
-    //   "isComplete": datauserbasicsService.isComplete,
-    //   "status": datauserbasicsService.status,
-    // }];
+    const data = [{
+      "createdAt": datauserbasicsService.createdAt,
+      "areas": areas,
+      "group": datagroup,
+      "division": datadivision,
+      "country": countri,
+      "gender": datauserbasicsService.gender,
+      "idProofNumber": datauserbasicsService.idProofNumber,
+      "city": citi,
+      "mobileNumber": datauserbasicsService.mobileNumber,
+      "roles": datauserauthsService.roles,
+      "fullName": datauserbasicsService.fullName,
+      "bio": datauserbasicsService.bio,
+      "avatar": mediaprofilepicts_res,
+      "isIdVerified": datauserbasicsService.isIdVerified,
+      "isEmailVerified": datauserauthsService.isEmailVerified,
+      "idProofStatus": datauserbasicsService.idProofStatus,
+      "insight": insights_res,
+      "langIso": languages.langIso,
+      "interest": interest,
+      "dob": datauserbasicsService.dob,
+      "event": datauserbasicsService.event,
+      "email": datauserbasicsService.email,
+      "username": datauserauthsService.username,
+      "isComplete": datauserbasicsService.isComplete,
+      "status": datauserbasicsService.status,
+    }];
 
     return {
       response_code: 202,
