62cea8ff829ff91eb7e0f6930449d0fc8e233c4e
sukmahyppe <sukma@hyppe.id>
2022-10-12 11:54:45 +0700

    update

--- src/content/posts/postcontent.service.ts
+++ src/content/posts/postcontent.service.ts
@@ -664,7 +664,7 @@ export class PostContentService {
     playlist.postType = post.postType;
     playlist.mediaId = Object(cm.oid);
     this.logger.log('createNewPostVideo >>> generate playlist ' + JSON.stringify(playlist));
-    this.postService.generateUserPlaylist(playlist);
+    //this.postService.generateUserPlaylist(playlist);
   }
 
   async getUserPost(body: any, headers: any): Promise<PostResponseApps> {
@@ -1921,7 +1921,7 @@ export class PostContentService {
     updatePl.userPostId = Object(profile._id);
     updatePl.mediaId = Object(cm.oid);
     updatePl.postType = apost.postType;
-    this.postService.updateGenerateUserPlaylist_(updatePl);
+    //this.postService.updateGenerateUserPlaylist_(updatePl);
 
     res.response_code = 202;
     let msg = new Messages();
