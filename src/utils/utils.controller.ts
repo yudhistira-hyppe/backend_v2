e084816c9b150378916b48695baf57ff3d62a818
sukmahyppe <sukma@hyppe.id>
2022-10-26 16:44:27 +0700

    update

--- src/utils/utils.controller.ts
+++ src/utils/utils.controller.ts
@@ -237,6 +237,7 @@ export class UtilsController {
         });
         var Response = {
             response_code: 202,
+            total: data.length.toString(),
             data: data_[0],
             messages: {
                 info: [
