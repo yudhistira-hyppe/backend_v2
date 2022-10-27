2a1b174c4fa72f7a48019fde517c9258a03e7f30
sukmahyppe <sukma@hyppe.id>
2022-10-27 17:56:04 +0700

    update

--- src/content/contentevents/contentevents.controller.ts
+++ src/content/contentevents/contentevents.controller.ts
@@ -315,14 +315,17 @@ export class ContenteventsController {
             error,
           );
         }
+      }else{
+        await this.contenteventsService.updateUnlike(email_user, "LIKE", "DONE", request.body.postID, true);
+        await this.contenteventsService.updateUnlike(email_receiverParty, "LIKE", "ACCEPT", request.body.postID,true);
       }
     } else if (eventType == "UNLIKE") {
       var ceck_data_DONE = await this.contenteventsService.ceckData(email_user, "LIKE", "DONE", email_receiverParty, "", request.body.postID);
       var ceck_data_ACCEPT = await this.contenteventsService.ceckData(email_receiverParty, "LIKE", "ACCEPT", "", email_user, request.body.postID);
       if ((await this.utilsService.ceckData(ceck_data_DONE)) && (await this.utilsService.ceckData(ceck_data_ACCEPT))) {
         try {
-          await this.contenteventsService.updateUnlike(email_user, "LIKE", "DONE", request.body.postID);
-          await this.contenteventsService.updateUnlike(email_receiverParty, "LIKE", "ACCEPT", request.body.postID);
+          await this.contenteventsService.updateUnlike(email_user, "LIKE", "DONE", request.body.postID,false);
+          await this.contenteventsService.updateUnlike(email_receiverParty, "LIKE", "ACCEPT", request.body.postID, false);
           await this.postsService.updateUnLike(email_receiverParty, request.body.postID);
         } catch (error) {
           await this.errorHandler.generateNotAcceptableException(
