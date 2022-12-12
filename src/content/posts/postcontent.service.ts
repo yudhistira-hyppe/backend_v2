774b29acc04dbfe91d9e64dbf30ccb4847ceb1bd
Yose <yose.adrian@hyppe.id>
2022-12-12 09:11:27 +0700

    status call back

--- src/content/posts/postcontent.service.ts
+++ src/content/posts/postcontent.service.ts
@@ -3036,26 +3036,20 @@ export class PostContentService {
     let resDiary: PostData[] = [];
     let resStory: PostData[] = [];
 
-    this.logger.log('getUserPostLandingPage >>> exec: video');
-    body.postType = 'vid';
-    body.withExp = false;
-    let pv = await this.doGetUserPost(body, headers, profile);
-    let pdv = await this.loadPostDataBulk(pv, body, profile, vids, pics, user);
-    for (let i = 0; i < pdv.length; i++) {
-      let ps = pdv[i];
-      posts.push(ps.postID);
+    if (String(body.postType) == 'pict') {
+      this.logger.log('getUserPostLandingPage >>> exec: pict');
+      body.postType = 'pict';
+      let pp = await this.doGetUserPost(body, headers, profile);
+      let pdp = await this.loadPostDataBulk(pp, body, profile, vids, pics, user);
+      for (let i = 0; i < pdp.length; i++) {
+        let ps = pdp[i];
+        posts.push(ps.postID);
+      }    
+      data.pict = pdp;
     }
-    data.video = pdv;
 
-    this.logger.log('getUserPostLandingPage >>> exec: pict');
-    body.postType = 'pict';
-    let pp = await this.doGetUserPost(body, headers, profile);
-    let pdp = await this.loadPostDataBulk(pp, body, profile, vids, pics, user);
-    for (let i = 0; i < pdp.length; i++) {
-      let ps = pdp[i];
-      posts.push(ps.postID);
-    }    
-    data.pict = pdp;
+
+
 
     this.logger.log('getUserPostLandingPage >>> exec: diary');
     body.postType = 'diary';
