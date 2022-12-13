5189691003bd711c6a6c8e16707f6d69900bb94a
sukmahyppe <sukma@hyppe.id>
2022-12-13 11:08:24 +0700

    update

--- src/utils/utils.service.ts
+++ src/utils/utils.service.ts
@@ -360,7 +360,7 @@ export class UtilsService {
       createNotificationsDto.contentEventID = null;
       createNotificationsDto.senderOrReceiverInfo = senderreceiver;
 
-      if (eventType == "LIKE" || eventType == "REACTION" || eventType == "APPEAL" || eventType == "TRANSACTION" || eventType == "CONTENT") {
+      if (eventType == "LIKE" || eventType == "REACTION" || eventType == "APPEAL" || eventType == "TRANSACTION" || eventType == "CONTENT" || eventType == "POST") {
         if (postID != undefined) {
           createNotificationsDto.postID = postID;
         }
