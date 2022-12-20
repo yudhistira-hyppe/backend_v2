f0b51e05fcb0e2ebd7465b66179e0047ade36f79
Yose <yose.adrian@hyppe.id>
2022-12-20 17:47:08 +0700

    disquss

--- src/content/disqus/disqus.service.ts
+++ src/content/disqus/disqus.service.ts
@@ -344,22 +344,37 @@ export class DisqusService {
                                                         $in: ['$_id', '$$localID']
                                                     }
                                                 },
-                                                {
-                                                    "senderActive": {
-                                                        $ne:false
-                                                    }
-                                                },
+                                                                                    {
+                                                                                            "receiver": "ilhamarahman97@gmail.com"
+                                                                                    },
                                                 {
                                                     "recieverActive": {
-                                                        $ne:false
+                                                        $ne: false
                                                     }
                                                 },
                                                 
                                             ]
                                         },
-                                        
+                                        {
+                                            $and: [
+                                                {
+                                                    $expr: {
+                                                        $in: ['$_id', '$$localID']
+                                                    }
+                                                },
+                                                                                    {
+                                                                                            "sender": "ilhamarahman97@gmail.com"
+                                                                                    },
+                                                {
+                                                    "senderActive": {
+                                                        $ne: false
+                                                    }
+                                                },
+                                                
+                                            ]
+                                        },
                                     ]
-                                },
+                                }
                                 
                             },
                             {
