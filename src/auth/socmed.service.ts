64369049e3b5aadf0f53262096c858386810b0af
sukmahyppe <sukma@hyppe.id>
2023-01-06 17:09:31 +0700

    update

--- src/auth/socmed.service.ts
+++ src/auth/socmed.service.ts
@@ -70,7 +70,7 @@ export class SocmedService {
 
   async signupsosmed(req: any) {
     this.logger.log("signupsosmedv2 >>> start: ");
-    console.log("signupsosmedv2 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> PARAM: ",JSON.stringify(req.body));
+    console.log("signupsosmedv2 >>> PARAM: "+JSON.stringify(req.body));
     var user_email = null;
     var user_socmedSource = null;
     var user_deviceId = null;
