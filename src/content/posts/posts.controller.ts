4ea67a6b82a2705bc145a58364646867392a9d72
sukmahyppe <sukma@hyppe.id>
2023-02-23 16:36:27 +0700

    update

--- src/content/posts/posts.controller.ts
+++ src/content/posts/posts.controller.ts
@@ -849,6 +849,7 @@ export class PostsController {
     return await this.bootsService.sendSoketAds(headers['x-auth-user']);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('api/posts/notifyapsara/cmod/image')
   async notifyApsaraCmodImage(@Body() body, @Headers() headers) {
     this.logger.log("notifyApsaraCmodImage >>> start: " + JSON.stringify(body));
