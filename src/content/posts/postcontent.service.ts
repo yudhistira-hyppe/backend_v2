47d6ddff5b593a8f8b6a38afaf7a6ccd9708a645
sukmahyppe <sukma@hyppe.id>
2023-05-24 17:43:51 +0700

    update

--- src/content/posts/postcontent.service.ts
+++ src/content/posts/postcontent.service.ts
@@ -2595,8 +2595,21 @@ export class PostContentService {
       }
     }
 
+    var file_resize = null;
+    if (image_format == "heif") {
+      console.log("heif", "true");
+      file_resize = await convert({
+        buffer: file.buffer,
+        format: 'JPEG',
+        quality: 1
+      });
+    } else {
+      console.log("heif", "false");
+      file_resize = file;
+    }
+
     //Convert Image
-    const buffers_file = await webp.buffer2webpbuffer(file, format, "-q 70", this.configService.get("PATH_UPLOAD"));
+    const buffers_file = await webp.buffer2webpbuffer(file_resize.buffer, format, "-q 70", this.configService.get("PATH_UPLOAD"));
     var file_commpress = buffers_file;
 
     //Convert Image Orientation
