ec57c191ca95988d3d6ee42d675c1e8046b27bfd
sukmahyppe <sukma@hyppe.id>
2023-06-06 11:38:36 +0700

    update module Task

--- src/schedule/task/task.module.ts
+++ src/schedule/task/task.module.ts
@@ -6,11 +6,9 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { UtilsModule } from '../../utils/utils.module';
 import { UserticketsModule } from '../../trans/usertickets/usertickets.module';
 import { UserbasicsModule } from '../../trans/userbasics/userbasics.module';
-import { PostsModule } from 'src/content/posts/posts.module';
 
 @Module({
   imports: [
-      PostsModule,
       ScheduleModule.forRoot()
     ],
   controllers: [TaskController],
