3a9d0b20e4f2e5dfdbd6727af47f32304fb52fc6
Kusnur1234 <kusnur@hyppe.id>
2023-06-27 16:07:05 +0700

    add userbadge
    Signed-off-by: Kusnur1234 <kusnur@hyppe.id>

--- src/trans/userbadge/userbadge.controller.ts
+++ src/trans/userbadge/userbadge.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res, UseGuards, Request, BadRequestException, HttpStatus, Req, HttpCode } from '@nestjs/common';
+import { UserbadgeService } from './userbadge.service';
+import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
+import { Userbadge } from './schemas/userbadge.schema';
+import { ErrorHandler } from '../../utils/error.handler';
+import { UtilsService } from '../../utils/utils.service';
+import mongoose, { mongo } from 'mongoose';
 
-@Controller('userbadge')
-export class UserbadgeController {}
+
+@Controller('api/userbadge')
+export class UserbadgeController {
+
+    constructor(private readonly userbadgeService: UserbadgeService,
+        private readonly utils: UtilsService) { }
+
+}
