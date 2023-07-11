2eff76049de54812978af678607fecb8b56a42d2
Kusnur1234 <kusnur@hyppe.id>
2023-07-11 14:18:32 +0700

    add detail collection badge
    Signed-off-by: Kusnur1234 <kusnur@hyppe.id>

--- src/trans/challenge/challenge.controller.ts
+++ src/trans/challenge/challenge.controller.ts
@@ -2040,6 +2040,45 @@ export class ChallengeController {
     }
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Post('listbadgeuserdetail')
+  async listbadgeuserdetail(@Req() request: Request) {
+    var iduser = null;
+    var page = null;
+    var limit = null;
+    var datasession = null;
+    var data = null;
+    var totalSession = null;
+    var request_json = JSON.parse(JSON.stringify(request.body));
+
+
+    if (request_json["iduser"] !== undefined) {
+      iduser = request_json['iduser'];
+    } else {
+      throw new BadRequestException("Unabled to proceed, ascending field is required");
+    }
+
+    page = request_json['page'];
+    limit = request_json['limit'];
+    try {
+      data = await this.userbadgeService.getBadgeUserCollection(iduser, page, limit);
+    } catch (e) {
+      data = [];
+    }
+
+
+    const messages = {
+      "info": ["The proses successful"],
+    };
+
+    return {
+      response_code: 202,
+      "data": data,
+      "page": page,
+      "limit": limit,
+      "message": messages
+    }
+  }
   @Post('userbadge')
   async userbadges() {
 
