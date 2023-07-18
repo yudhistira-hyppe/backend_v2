0b518a5d9470fa6d4437d1610c94e1c95d09c80b
Kusnur1234 <kusnur@hyppe.id>
2023-07-18 13:26:17 +0700

    update getuserbadge
    Signed-off-by: Kusnur1234 <kusnur@hyppe.id>

--- src/trans/challenge/subChallenge.service.ts
+++ src/trans/challenge/subChallenge.service.ts
@@ -5494,7 +5494,25 @@ export class subChallengeService {
                                 ]
                             },
                             then: "BERLANGSUNG",
-                            else: "BERAKHIR"
+                            else:
+                            {
+                                $cond: {
+                                    if: {
+                                        $and: [
+                                            {
+                                                $lt:
+                                                    [
+                                                        "$endDatetime",
+                                                        "$timenow",
+                                                    ]
+                                            },
+
+                                        ]
+                                    },
+                                    else: "AKAN DATANG",
+                                    then: "BERAKHIR"
+                                }
+                            },
                         }
                     },
 
