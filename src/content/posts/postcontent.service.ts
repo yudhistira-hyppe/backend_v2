d1fafd668bdea5c9d96df99cfdb3e9a965757234
sukmahyppe <sukma@hyppe.id>
2023-08-01 15:01:33 +0700

    update

--- src/content/posts/postcontent.service.ts
+++ src/content/posts/postcontent.service.ts
@@ -1661,6 +1661,18 @@ export class PostContentService {
     //Create Response
     let CreatePostResponse_ = new CreatePostResponse();
     let dataResponse = await this.postService.findByPostId(Posts_._id.toString());
+    var avatar = await this.utilService.getAvatarUser(Posts_.email.toString());
+    var username = await this.utilService.getUsertname(Posts_.email.toString());
+    dataResponse["username"] = username; 
+    dataResponse["avatar"] = avatar; 
+    dataResponse["isLiked"] = false;
+    dataResponse["privacy"] = {
+      isCelebrity: (data_userbasics.isCelebrity != undefined) ? data_userbasics.isCelebrity : false,
+      isIdVerified: (data_userbasics.isIdVerified != undefined) ? data_userbasics.isIdVerified : false,
+      isPrivate: (data_userbasics.isPrivate != undefined) ? data_userbasics.isPrivate : false,
+      isFollowPrivate: (data_userbasics.isFollowPrivate != undefined) ? data_userbasics.isFollowPrivate : false,
+      isPostPrivate: (data_userbasics.isPostPrivate != undefined) ? data_userbasics.isPostPrivate : false,
+    };
     let Messages_ = new Messages();
     Messages_.info = ["The process successful"];
     CreatePostResponse_.messages = Messages_;
@@ -3140,14 +3152,24 @@ export class PostContentService {
 
     //Create Response
     let CreatePostResponse_ = new CreatePostResponse();
+    let dataResponse = await this.postService.findByPostId(Posts_._id.toString());
+    var avatar = await this.utilService.getAvatarUser(Posts_.email.toString());
+    var username = await this.utilService.getUsertname(Posts_.email.toString());
+    dataResponse["username"] = username;
+    dataResponse["avatar"] = avatar;
+    dataResponse["isLiked"] = false;
+    dataResponse["privacy"] = {
+      isCelebrity: (data_userbasics.isCelebrity != undefined) ? data_userbasics.isCelebrity : false,
+      isIdVerified: (data_userbasics.isIdVerified != undefined) ? data_userbasics.isIdVerified : false,
+      isPrivate: (data_userbasics.isPrivate != undefined) ? data_userbasics.isPrivate : false,
+      isFollowPrivate: (data_userbasics.isFollowPrivate != undefined) ? data_userbasics.isFollowPrivate : false,
+      isPostPrivate: (data_userbasics.isPostPrivate != undefined) ? data_userbasics.isPostPrivate : false,
+    };
     let Messages_ = new Messages();
     Messages_.info = ["The process successful"];
     CreatePostResponse_.messages = Messages_;
     CreatePostResponse_.response_code = 202;
-    var pd = new PostData();
-    pd.postID = String(dataPost.postID);
-    pd.email = String(dataPost.email);
-    CreatePostResponse_.data = pd;
+    CreatePostResponse_.data = dataResponse;
 
     console.log('============================================== CREATE POST END ==============================================', JSON.stringify(CreatePostResponse_));
     return CreatePostResponse_;
