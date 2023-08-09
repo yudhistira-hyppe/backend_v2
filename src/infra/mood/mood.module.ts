9b03020b1941301631244c68a2da15946dcba849
hipSalman <salman@hyppe.id>
2023-08-09 17:40:18 +0700

    penambahan fitur logapi di mood dan theme controller

--- src/infra/mood/mood.module.ts
+++ src/infra/mood/mood.module.ts
@@ -5,9 +5,11 @@ import { MoodController } from './mood.controller';
 import { MoodService } from './mood.service';
 import { Mood, MoodSchema } from './schemas/mood.schema';
 import { UtilsModule } from '../../utils/utils.module';
+import { LogapisModule } from 'src/trans/logapis/logapis.module'; 
 
 @Module({
     imports: [
+        LogapisModule,
         UtilsModule,
         ConfigModule.forRoot(),
         MongooseModule.forFeature([{ name: Mood.name, schema: MoodSchema }], 'SERVER_FULL')
