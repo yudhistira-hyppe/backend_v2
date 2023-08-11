29f97b8cd82b825d07818fbfb884a51acd78873f
hipSalman <salman@hyppe.id>
2023-08-11 17:12:00 +0700

    penambahan function logapi di leveltickets controller

--- src/trans/leveltickets/leveltickets.module.ts
+++ src/trans/leveltickets/leveltickets.module.ts
@@ -4,10 +4,14 @@ import { LevelticketsController } from './leveltickets.controller';
 import { LevelticketsService } from './leveltickets.service';
 import { ConfigModule } from '@nestjs/config';
 import { Leveltickets, LevelticketsSchema } from './schemas/leveltickets.schema';
+import { LogapisModule } from '../logapis/logapis.module';
+import { UtilsModule } from 'src/utils/utils.module'; 
 
 @Module({
 
     imports: [
+        UtilsModule,
+        LogapisModule,
         ConfigModule.forRoot(),
         MongooseModule.forFeature([{ name: Leveltickets.name, schema: LevelticketsSchema }], 'SERVER_FULL')
     ],
