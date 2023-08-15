ad60f7387369d68fcd14bd9863eaa39454e44407
hipSalman <salman@hyppe.id>
2023-08-15 14:16:33 +0700

    penambahan function logapi di user management controller

--- src/trans/usermanagement/module/module.module.ts
+++ src/trans/usermanagement/module/module.module.ts
@@ -6,10 +6,12 @@ import { ModuleService } from './module.service';
 import { ConfigModule } from '@nestjs/config';
 import { Module as Module_, ModuleSchema } from './schemas/module.schema';
 import { UtilsModule } from '../../../utils/utils.module';
+import { LogapisModule } from 'src/trans/logapis/logapis.module';
 
 @Module({
 
     imports: [
+        LogapisModule,
         UtilsModule,
         ConfigModule.forRoot(),
         MongooseModule.forFeature([{ name: Module_.name, schema: ModuleSchema }], 'SERVER_FULL')
