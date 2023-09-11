ba3dc23180d1e9ca13a6e44dc0c9889dceff9561
Kusnur1234 <kusnur@hyppe.id>
2023-09-11 14:30:34 +0700

    update ldp
    Signed-off-by: Kusnur1234 <kusnur@hyppe.id>

--- src/content/mediastiker/countstiker.service.ts
+++ src/content/mediastiker/countstiker.service.ts
@@ -14,11 +14,9 @@ export class CountstikerService {
         private readonly MediastikerSS: MediastikerService
     ) { }
 
-    async updatedata(list:any[], target:string, operathmath:string)
-    {
+    async updatedata(list: any[], target: string, operathmath: string) {
         if (list !== undefined) {
-            for(let i = 0; i < list.length; i++)
-            {
+            for (let i = 0; i < list.length; i++) {
                 setTimeout(() => {
                     console.log('looping ke ' + i);
                     this.intothedatabase2(i, target, operathmath, list);
@@ -27,17 +25,15 @@ export class CountstikerService {
         }
     }
 
-    async intothedatabase2(loop:number, target:string, operathmath:string, list:any[])
-    {
+    async intothedatabase2(loop: number, target: string, operathmath: string, list: any[]) {
         var operationresult = null;
         var mongo = require('mongoose');
         var konvertid = mongo.Types.ObjectId(list[loop]._id);
-        var data = await this.CountstikerModel.findOne({ stikerId:konvertid });
+        var data = await this.CountstikerModel.findOne({ stikerId: konvertid });
 
         var setdata = new Countstiker();
 
-        if(data == null)
-        {
+        if (data == null) {
             var getdata = await this.MediastikerSS.findOne(list[loop]._id);
             setdata._id = mongo.Types.ObjectId();
             setdata.stikerId = konvertid;
@@ -47,52 +43,105 @@ export class CountstikerService {
             setdata.countused = (target == "used" ? 1 : 0);
             setdata.createdAt = getdata.createdAt;
         }
-        else
-        {
-            if(target == "search")
-            {
+        else {
+            if (target == "search") {
                 setdata.countsearch = (operathmath == "penjumlahan" ? data.countsearch + 1 : data.countsearch - 1);
-                if(setdata.countsearch < 0)
-                {
+                if (setdata.countsearch < 0) {
                     setdata.countsearch = 0;
                 }
             }
-            else
-            {
+            else {
                 setdata.countused = (operathmath == "penjumlahan" ? data.countused + 1 : data.countused - 1);
-                if(setdata.countused < 0)
-                {
+                if (setdata.countused < 0) {
                     setdata.countused = 0;
                 }
             }
         }
 
-        try
-        {
-            if(setdata._id == null)
-            {
+        try {
+            if (setdata._id == null) {
                 //stuck disini
                 operationresult = await this.CountstikerModel.updateOne(
-                    { 
-                        _id:konvertid 
+                    {
+                        _id: konvertid
                     },
                     data,
                     function (err, docs) {
                         if (err) {
-                        console.log(err);
+                            console.log(err);
                         } else {
-                        console.log(docs);
+                            console.log(docs);
                         }
                     },
                 );
             }
-            else
-            {
+            else {
                 operationresult = await this.CountstikerModel.create(data);
             }
         }
-        catch(e)
-        {
+        catch (e) {
+            console.log(e);
+        }
+
+        console.log(operationresult);
+        console.log(konvertid);
+    }
+
+    async searchstikerlog(loop: number, target: string, operathmath: string, list: any[]) {
+        var operationresult = null;
+        var mongo = require('mongoose');
+        var konvertid = mongo.Types.ObjectId(list[loop]._id);
+        var data = await this.CountstikerModel.findOne({ stikerId: konvertid });
+
+        var setdata = new Countstiker();
+
+        if (data == null) {
+            var getdata = await this.MediastikerSS.findOne(list[loop]._id);
+            setdata._id = mongo.Types.ObjectId();
+            setdata.stikerId = konvertid;
+            setdata.name = getdata.name;
+            setdata.image = getdata.image;
+            setdata.countsearch = (target == "search" ? 1 : 0);
+            setdata.countused = (target == "used" ? 1 : 0);
+            setdata.createdAt = getdata.createdAt;
+        }
+        else {
+            if (target == "search") {
+                setdata.countsearch = (operathmath == "penjumlahan" ? data.countsearch + 1 : data.countsearch - 1);
+                if (setdata.countsearch < 0) {
+                    setdata.countsearch = 0;
+                }
+            }
+            else {
+                setdata.countused = (operathmath == "penjumlahan" ? data.countused + 1 : data.countused - 1);
+                if (setdata.countused < 0) {
+                    setdata.countused = 0;
+                }
+            }
+        }
+
+        try {
+            if (setdata._id == null) {
+                //stuck disini
+                operationresult = await this.CountstikerModel.updateOne(
+                    {
+                        _id: konvertid
+                    },
+                    data,
+                    function (err, docs) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log(docs);
+                        }
+                    },
+                );
+            }
+            else {
+                operationresult = await this.CountstikerModel.create(data);
+            }
+        }
+        catch (e) {
             console.log(e);
         }
 
