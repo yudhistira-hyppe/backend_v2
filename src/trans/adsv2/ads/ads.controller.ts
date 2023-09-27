c4ba1ce53905ee07412f3d5e0b1d458512e99a8d
sukmahyppe <sukma@hyppe.id>
2023-09-27 11:35:27 +0700

    update

--- src/trans/adsv2/ads/ads.controller.ts
+++ src/trans/adsv2/ads/ads.controller.ts
@@ -1014,55 +1014,55 @@ export class AdsController {
 
         try {
             let ads_campaign_dashboard = await this.adsService.campaignDashboard(body.userId, start_date, end_date);
-            //let ads_status_campaign_dashboard = await this.adsService.getAdsSatus(body.userId, start_date, end_date);
-            // if (await this.utilsService.ceckData(ads_campaign_dashboard)) {
-            //     if (ads_campaign_dashboard.length > 0) {
-            //         ads_campaign_dashboard = ads_campaign_dashboard[0];
-            //         ads_campaign_dashboard.statusIklan = ads_status_campaign_dashboard[0].status;
-            //     }
-            // }
-            // for (var d = start_date; d <= end_date; d.setDate(d.getDate() + 1)) {
-            //     var DateFormat = await this.utilsService.consvertDateTimeString(new Date(d));
-            //     const isFoundreach = ads_campaign_dashboard.reach.some(element => {
-            //         if (element._id === DateFormat) {
-            //             return true;
-            //         }
-            //         return false;
-            //     });
-            //     if (!isFoundreach) {
-            //         ads_campaign_dashboard.reach.push({
-            //             "_id": DateFormat,
-            //             "reachView": 0
-            //         })
-            //     }
-            //     const isFoundimpresi = ads_campaign_dashboard.impresi.some(element => {
-            //         if (element._id === DateFormat) {
-            //             return true;
-            //         }
-            //         return false;
-            //     });
-            //     if (!isFoundimpresi) {
-            //         ads_campaign_dashboard.impresi.push({
-            //             "_id": DateFormat,
-            //             "impresiView": 0
-            //         })
-            //     }
-            //     const isFoundCTA = ads_campaign_dashboard.CTA.some(element => {
-            //         if (element._id === DateFormat) {
-            //             return true;
-            //         }
-            //         return false;
-            //     });
-            //     if (!isFoundCTA) {
-            //         ads_campaign_dashboard.CTA.push({
-            //             "_id": DateFormat,
-            //             "CTACount": 0
-            //         })
-            //     }
-            // }
+            let ads_status_campaign_dashboard = await this.adsService.getAdsSatus(body.userId, start_date, end_date);
+            if (await this.utilsService.ceckData(ads_campaign_dashboard)) {
+                if (ads_campaign_dashboard.length > 0) {
+                    ads_campaign_dashboard = ads_campaign_dashboard[0];
+                    ads_campaign_dashboard.statusIklan = ads_status_campaign_dashboard[0].status;
+                }
+            }
+            for (var d = start_date; d <= end_date; d.setDate(d.getDate() + 1)) {
+                var DateFormat = await this.utilsService.consvertDateTimeString(new Date(d));
+                const isFoundreach = ads_campaign_dashboard.reach.some(element => {
+                    if (element._id === DateFormat) {
+                        return true;
+                    }
+                    return false;
+                });
+                if (!isFoundreach) {
+                    ads_campaign_dashboard.reach.push({
+                        "_id": DateFormat,
+                        "reachView": 0
+                    })
+                }
+                const isFoundimpresi = ads_campaign_dashboard.impresi.some(element => {
+                    if (element._id === DateFormat) {
+                        return true;
+                    }
+                    return false;
+                });
+                if (!isFoundimpresi) {
+                    ads_campaign_dashboard.impresi.push({
+                        "_id": DateFormat,
+                        "impresiView": 0
+                    })
+                }
+                const isFoundCTA = ads_campaign_dashboard.CTA.some(element => {
+                    if (element._id === DateFormat) {
+                        return true;
+                    }
+                    return false;
+                });
+                if (!isFoundCTA) {
+                    ads_campaign_dashboard.CTA.push({
+                        "_id": DateFormat,
+                        "CTACount": 0
+                    })
+                }
+            }
 
-            // var timestamps_end = await this.utilsService.getDateTimeString();
-            // this.logapiSS.create2(fullurl, timestamps_start, timestamps_end, headers['x-auth-user'], null, null, reqbody);
+            var timestamps_end = await this.utilsService.getDateTimeString();
+            this.logapiSS.create2(fullurl, timestamps_start, timestamps_end, headers['x-auth-user'], null, null, reqbody);
 
             return await this.errorHandler.generateAcceptResponseCodeWithData(
                 "Get Ads Campaign Dashboard succesfully", ads_campaign_dashboard,
