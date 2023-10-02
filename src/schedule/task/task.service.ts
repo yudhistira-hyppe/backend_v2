89e5af61af4476848fc09153565e007bef8bbd66
sukmahyppe <sukma@hyppe.id>
2023-10-02 14:06:55 +0700

    update

--- src/schedule/task/task.service.ts
+++ src/schedule/task/task.service.ts
@@ -18,10 +18,12 @@ export class TaskService {
   //   this.logger.debug('Called when the current second is 45');
   // }
 
-  @Cron('0 0 23 * * *')
-  seaweedJob() {
-    this.logger.debug('SEAWEED JOB START');
-    
+  @Cron('*/10 * * * * *')
+  challengeJob() {
+    this.logger.debug('Challenge JOB START');
+    this.challengeService.userbadge();
+    this.challengeService.updateUserbadge();
+    this.challengeService.sendNotifeChallenge();
     //this.postContentService.cronJobSeaweedProfileStart();
     //this.postContentService.cronJobSeaweedPictStart();
     //this.postContentService.cronJobSeaweedVidStart();
