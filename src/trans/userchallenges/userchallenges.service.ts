fa87710db87760fd0adf2f6b453f15712d66120e
Kusnur1234 <kusnur@hyppe.id>
2023-10-24 15:58:37 +0700

    update
    Signed-off-by: Kusnur1234 <kusnur@hyppe.id>

--- src/trans/userchallenges/userchallenges.service.ts
+++ src/trans/userchallenges/userchallenges.service.ts
@@ -16,12 +16,12 @@ export class UserchallengesService {
         return _Userchallenges_;
     }
 
-    async findData(userchallenges:Userchallenges): Promise<Userchallenges[]> {
+    async findData(userchallenges: Userchallenges): Promise<Userchallenges[]> {
         return this.UserchallengesModel.find(userchallenges).exec();
     }
 
-    async updateByUSer(id: string, userchallenges: Userchallenges) {
-        let result = await this.UserchallengesModel.updateOne({ _id: new Types.ObjectId(id) }, userchallenges).exec();
+    async updateByUSer(id: string, idSubChallenge: string, userchallenges: Userchallenges) {
+        let result = await this.UserchallengesModel.updateOne({ _id: new Types.ObjectId(id), idSubChallenge: new Types.ObjectId(idSubChallenge) }, userchallenges).exec();
         return result;
     }
 
