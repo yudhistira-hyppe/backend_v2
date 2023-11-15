07c1914e80163cd7ca44f8f8933c9acd128d26bb
hipSalman <salman@hyppe.id>
2023-11-15 16:27:28 +0700

    api boostmaster v2

--- src/content/boostsession/intervarsession/intervarsession.controller.ts
+++ src/content/boostsession/intervarsession/intervarsession.controller.ts
@@ -79,4 +79,60 @@ export class IntervalSessionController {
     }
     return Response;
   }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('api/post/boostmaster/v2')
+  @HttpCode(HttpStatus.ACCEPTED)
+  async getData2(@Headers() headers) {
+    if (headers['x-auth-user'] == undefined) {
+      await this.errorHandler.generateNotAcceptableException(
+        'Unauthorized',
+      );
+    }
+    if (!(await this.utilsService.validasiTokenEmail(headers))) {
+      await this.errorHandler.generateNotAcceptableException(
+        'Unabled to proceed email header dan token not match',
+      );
+    }
+    var profile = await this.utilsService.generateProfile2(headers['x-auth-user'], "FULL");
+    if (!(await this.utilsService.ceckData(profile))) {
+      await this.errorHandler.generateNotAcceptableException(
+        'Unabled to proceed user not found',
+      );
+    }
+    const langIso = (profile.langIso != undefined) ? profile.langIso : "id";
+    var BoostsessionDto_ = new BoostsessionDto();
+    BoostsessionDto_.type = "manual";
+    BoostsessionDto_.langIso = langIso;
+    var BoostintervalDto_ = new BoostintervalDto();
+    BoostintervalDto_.type = "manual";
+    BoostintervalDto_.langIso = langIso;
+    var interval_data = await this.boostintervalService.findWhere(BoostintervalDto_);
+    var session_data = await this.boostsessionService.findWhere(BoostsessionDto_);
+    //CECK PENDING TRANSACTION
+    var CountPending = 0;
+    var daftarPending = await this.transactionsService.findPendingByUser(profile.iduser.toString());
+    if ((await this.utilsService.ceckData(daftarPending))) {
+      let cekstatusva = await this.oyPgService.staticVaInfo(daftarPending.idva);
+      if (cekstatusva!=undefined){
+        if (cekstatusva.va_status === "WAITING_PAYMENT") {
+          CountPending=1;
+        }
+      }
+    }
+    var Response = {
+      response_code: 202,
+      data: {
+        pendingTransaction: CountPending,
+        interval: interval_data,
+        session: session_data,
+      },
+      messages: {
+        info: [
+          "Succesfully"
+        ]
+      }
+    }
+    return Response;
+  }
 }
