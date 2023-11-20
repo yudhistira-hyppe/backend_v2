db850b7df12c5a4ae16e391ff558570083ded6be
Kusnur1234 <kusnur@hyppe.id>
2023-11-20 11:50:25 +0700

    delete modul
    Signed-off-by: Kusnur1234 <kusnur@hyppe.id>

--- src/content/newnotification/newnotification.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { NewNotificationService } from './newnotification.service';
-
-@Controller('newnotification')
-export class NewNotificationController {
-  constructor(private readonly newnotificationService: NewNotificationService) {}
-}
