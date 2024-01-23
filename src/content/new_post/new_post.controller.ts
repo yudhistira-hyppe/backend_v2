68e79f563c46d9abbc679a8f744ac8902a21ecf7
hipSalman <salman@hyppe.id>
2024-01-23 11:59:05 +0700

    perbaikan api getuserposts

--- src/content/new_post/new_post.controller.ts
+++ src/content/new_post/new_post.controller.ts
@@ -1091,75 +1091,75 @@ export class NewPostController {
         // var data = await this.UserbasicnewService.getpostquery(email:string, visibility:string, postids: string, tipepost:string, activestatus:string, exptime:string, skip:number, page:number, insight:string, sorttime:string);
         var data = await this.newPostService.getpostquery(auth.email, body.search, body.visibility, body.postID, body.postType, body.withActive, body.withExp, setPagerow, setPagenumber, body.withInsight, 'true');
 
-        var listdatagambar = [];
-        var listdatavideo = [];
-        var listmusic = [];
-        var tempresult = null;
-        var tempdata = null;
-        if(data.length != 0)
-        {
-            for (var i = 0; i < data.length; i++) {
-                tempdata = data[i];
-                if (tempdata.isApsara == true) {
-                    var gettypedata = tempdata.mediaMime.split("/")[0];
-                    if(gettypedata == "video" || gettypedata == "videos")
-                    {
-                        listdatavideo.push(tempdata.apsaraId);
-                    }
-                    else
-                    {
-                        listdatagambar.push(tempdata.apsaraId);
-                    }
-                }
+        // var listdatagambar = [];
+        // var listdatavideo = [];
+        // var listmusic = [];
+        // var tempresult = null;
+        // var tempdata = null;
+        // if(data.length != 0)
+        // {
+        //     for (var i = 0; i < data.length; i++) {
+        //         tempdata = data[i];
+        //         if (tempdata.isApsara == true) {
+        //             var gettypedata = tempdata.mediaMime.split("/")[0];
+        //             if(gettypedata == "video" || gettypedata == "videos")
+        //             {
+        //                 listdatavideo.push(tempdata.apsaraId);
+        //             }
+        //             else
+        //             {
+        //                 listdatagambar.push(tempdata.apsaraId);
+        //             }
+        //         }
     
-                var getmusicapsara = null;
-                try {
-                    getmusicapsara = tempdata.music.apsaraThumnail;
-                }
-                catch (e) {
-                    getmusicapsara = undefined;
-                }
-                listmusic.push(getmusicapsara);
-            }
+        //         var getmusicapsara = null;
+        //         try {
+        //             getmusicapsara = tempdata.music.apsaraThumnail;
+        //         }
+        //         catch (e) {
+        //             getmusicapsara = undefined;
+        //         }
+        //         listmusic.push(getmusicapsara);
+        //     }
 
-            if(listdatagambar.length != 0)
-            {
-                // console.log(listdatagambar);
-                var apsaraimagedata = await this.newPostContentService.getImageApsara(listdatagambar);
-                // console.log(apsaraimagedata);
-                // console.log(resultdata.ImageInfo[0]);
-                tempresult = apsaraimagedata.ImageInfo;
-                for (var i = 0; i < data.length; i++) {
-                    for (var j = 0; j < tempresult.length; j++) {
-                        if (tempresult[j].ImageId == data[i].apsaraId) {
-                            data[i].apsaraThumbId = tempresult[j].apsaraThumbId;
-                        }
-                    }
-                    // if (resultquery[i].apsara == false && (resultquery[i].mediaType == "image" || resultquery[i].mediaType == "images")) {
-                    //     resultquery[i].apsaraThumbId = '/thumb/' + resultquery[i].postID;
-                    // }
-                }
-            }
+        //     if(listdatagambar.length != 0)
+        //     {
+        //         // console.log(listdatagambar);
+        //         var apsaraimagedata = await this.newPostContentService.getImageApsara(listdatagambar);
+        //         // console.log(apsaraimagedata);
+        //         // console.log(resultdata.ImageInfo[0]);
+        //         tempresult = apsaraimagedata.ImageInfo;
+        //         for (var i = 0; i < data.length; i++) {
+        //             for (var j = 0; j < tempresult.length; j++) {
+        //                 if (tempresult[j].ImageId == data[i].apsaraId) {
+        //                     data[i].apsaraThumbId = tempresult[j].apsaraThumbId;
+        //                 }
+        //             }
+        //             // if (resultquery[i].apsara == false && (resultquery[i].mediaType == "image" || resultquery[i].mediaType == "images")) {
+        //             //     resultquery[i].apsaraThumbId = '/thumb/' + resultquery[i].postID;
+        //             // }
+        //         }
+        //     }
 
-            if(listdatavideo.length != 0)
-            {
-                // console.log(listdatavideo);
-                var apsaravideodata = await this.newPostContentService.getVideoApsara(listdatavideo);
-                // console.log(apsaravideodata);
-                // console.log(resultdata.ImageInfo[0]);
-                tempresult = apsaravideodata.VideoList;
-                for (var i = 0; i < data.length; i++) {
-                    for (var j = 0; j < tempresult.length; j++) {
-                        if (tempresult[j].VideoId == data[i].apsaraId) {
-                            data[i].mediaThumbEndpoint = tempresult[j].CoverURL;
-                        }
-                    }
-                    // if (resultquery[i].apsara == false && resultquery[i].mediaType == "video") {
-                    //     resultquery[i].mediaThumbEndpoint = '/thumb/' + resultquery[i].postID;
-                    // }
-                }
-            }
-        }
+        //     if(listdatavideo.length != 0)
+        //     {
+        //         // console.log(listdatavideo);
+        //         var apsaravideodata = await this.newPostContentService.getVideoApsara(listdatavideo);
+        //         // console.log(apsaravideodata);
+        //         // console.log(resultdata.ImageInfo[0]);
+        //         tempresult = apsaravideodata.VideoList;
+        //         for (var i = 0; i < data.length; i++) {
+        //             for (var j = 0; j < tempresult.length; j++) {
+        //                 if (tempresult[j].VideoId == data[i].apsaraId) {
+        //                     data[i].mediaThumbEndpoint = tempresult[j].CoverURL;
+        //                 }
+        //             }
+        //             // if (resultquery[i].apsara == false && resultquery[i].mediaType == "video") {
+        //             //     resultquery[i].mediaThumbEndpoint = '/thumb/' + resultquery[i].postID;
+        //             // }
+        //         }
+        //     }
+        // }
 
         // console.log(listmusic);
         // var apsaramusic = await this.musicSS.getImageApsara(listmusic);
