c2d3a8c2577b8943918415b8eb95cc28f276285e
sukmahyppe <sukma@hyppe.id>
2024-01-31 13:16:07 +0700

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as fs from 'fs';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,19 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('images/:id')
+  async downloadFile(
+    @Param('id') id: string, @Res() response) {
+    const mime = require('mime-types')
+    const pathUpload = "../../images/";
+    const mime_type = mime.lookup(pathUpload + id)
+    var file = fs.createReadStream(pathUpload + id);
+    var stat = fs.statSync(pathUpload + id);
+    response.setHeader('Content-Length', stat.size);
+    response.setHeader('Content-Type', mime_type);
+    response.setHeader('Content-Disposition', 'attachment; filename=' + id);
+    file.pipe(response);
   }
 }
