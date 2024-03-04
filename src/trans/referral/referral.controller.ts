0b7cd2455aebd8012adb89dbf5b7c648aadc3c0f
haris-dwi-prakoso <harisdwi.prakoso@gmail.com>
2024-03-04 09:27:31 +0700

    list referrals
    Signed-off-by: haris-dwi-prakoso <harisdwi.prakoso@gmail.com>

--- src/trans/referral/referral.controller.ts
+++ src/trans/referral/referral.controller.ts
@@ -1,31 +1,38 @@
-import { Body, Controller, Delete, Get, Param, Post,UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, UseGuards, Req } from '@nestjs/common';
 import { ReferralService } from './referral.service';
 import { CreateReferralDto } from './dto/create-referral.dto';
 import { Referral } from './schemas/referral.schema';
 import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
 @Controller('api/referral')
 export class ReferralController {
- 
-        constructor(private readonly referralService: ReferralService) {}
-    
-        @Post()
-        async create(@Body() CreateSagasDto: CreateReferralDto) {
-          await this.referralService.create(CreateSagasDto);
-        }
-        @UseGuards(JwtAuthGuard)
-        @Get()
-        async findAll(): Promise<Referral[]> {
-          return this.referralService.findAll();
-        }
-      
-        @Get(':id')
-        async findOne(@Param('id') id: string): Promise<Referral> {
-          return this.referralService.findOne(id);
-        }
-      
-        @Delete(':id')
-        async delete(@Param('id') id: string) {
-          return this.referralService.delete(id);
-        }
+
+  constructor(private readonly referralService: ReferralService) { }
+
+  @Post()
+  async create(@Body() CreateSagasDto: CreateReferralDto) {
+    await this.referralService.create(CreateSagasDto);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async findAll(): Promise<Referral[]> {
+    return this.referralService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<Referral> {
+    return this.referralService.findOne(id);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.referralService.delete(id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('/list')
+  async list(@Req() request: Request) {
+    var request_json = JSON.parse(JSON.stringify(request.body));
+    return this.referralService.listAll(request_json.email, request_json.from, request_json.to);
+  }
 
 }
--- src/trans/referral/referral.service.ts
+++ src/trans/referral/referral.service.ts
@@ -58,4 +58,132 @@ export class ReferralService {
       .exec();
     return deletedCat;
   }
+
+  async listAll(parentEmail: string, fromDate?: string, toDate?: string) {
+    let dataPipeline = [];
+    dataPipeline.push({
+      "$match": {
+        "parent": parentEmail
+      }
+    })
+    if (fromDate && fromDate !== undefined) {
+      dataPipeline.push({
+        "$match": {
+          "createdAt": {
+            $gte: fromDate
+          }
+        }
+      })
+    }
+    if (toDate && toDate !== undefined) {
+      dataPipeline.push({
+        "$match": {
+          "createdAt": {
+            $lte: toDate
+          }
+        }
+      })
+    }
+    dataPipeline.push(
+      {
+        "$sort": {
+          "createdAt": -1
+        }
+      },
+      {
+        "$lookup": {
+          from: "newUserBasics",
+          localField: "children",
+          foreignField: "email",
+          as: "childData"
+        }
+      },
+      {
+        "$project": {
+          parent: 1,
+          children: 1,
+          active: 1,
+          verified: 1,
+          imei: 1,
+          createdAt: 1,
+          updatedAt: 1,
+          childFullName: {
+            $arrayElemAt: ['$childData.fullName', 0]
+          },
+          childDOB: {
+            $arrayElemAt: ['$childData.dob', 0]
+          },
+          childAge: {
+            "$ifNull": [
+              {
+                "$dateDiff": {
+                  "startDate": {
+                    $dateFromString: {
+                      dateString: {
+                        $arrayElemAt: ['$childData.dob', 0]
+                      }
+                    }
+                  },
+                  "endDate": "$$NOW",
+                  "unit": "year"
+                }
+              },
+              null
+            ]
+          },
+          childGender: {
+            $arrayElemAt: ['$childData.gender', 0]
+          },
+          childCity: {
+            $arrayElemAt: ['$childData.citiesName', 0]
+          },
+          childState: {
+            $arrayElemAt: ['$childData.statesName', 0]
+          },
+          childAvatar: {
+            mediaBasePath: {
+              $arrayElemAt: ['$childData.mediaBasePath', 0]
+            },
+            mediaUri: {
+              $arrayElemAt: ['$childData.mediaUri', 0]
+            },
+            mediaEndpoint: {
+              $arrayElemAt: ['$childData.mediaEndpoint', 0]
+            }
+          }
+        }
+      }
+    )
+    let data = await this.referralModel.aggregate([
+      {
+        "$facet": {
+          total: [
+            {
+              "$match": {
+                "parent": parentEmail
+              }
+            },
+            {
+              "$group": {
+                _id: "$parent",
+                total: {
+                  $sum: 1
+                }
+              }
+            }
+          ],
+          data: dataPipeline
+        }
+      },
+      {
+        "$project": {
+          total: {
+            $arrayElemAt: ['$total.total', 0]
+          },
+          data: 1
+        }
+      }
+    ])
+    return data;
+  }
 }
