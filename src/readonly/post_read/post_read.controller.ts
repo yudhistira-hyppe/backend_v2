538aef3839a3991f7d6b6aab82f411dcc58ccc5c
kusnur629 <kusnur9@gmail.com>
2024-04-16 17:00:54 +0700

    update byprofile/v2

--- src/readonly/post_read/post_read.controller.ts
+++ src/readonly/post_read/post_read.controller.ts
@@ -902,7 +902,8 @@ export class PostsReadController {
         try {
 
             // data = await this.postsReadService.landingpageMy2V2(email, postType, parseInt(pageNumber), parseInt(pageRow), emailLogin);
-            data = await this.post2SS.landingpageMigration(email, emailLogin, postType, postid, visibility, active, exp, withinsight, parseInt(pageNumber), parseInt(pageRow))
+           // data = await this.post2SS.landingpageMigration(email, emailLogin, postType, postid, visibility, active, exp, withinsight, parseInt(pageNumber), parseInt(pageRow))
+            data = await this.post2SS.landingpageMigration(email, emailLogin, postType, postid, visibility, active, exp, withinsight, parseInt(pageNumber),30)
             lengpict = data.length;
             console.log("data", data);
         } catch (e) {
